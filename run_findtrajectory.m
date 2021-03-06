ea3ee3fe1dc742e808eeb650e21ffe7c9c378267
hkolano <hannah.kolano@students.olin.edu>
2021-03-06 14:49:58 -0600

    Faster velocity limit checking

--- run_findtrajectory.m
+++ run_findtrajectory.m
@@ -4,12 +4,12 @@
 %         -164.61, 35.39; ...
 %         -165, 165; ...
 %         -180, 180]*pi/180;
-theta_1 = [0 0 0 0 0].'*pi/180;
+theta_1 = [0 100 0 0 0].'*pi/180;
 theta_2 = [10 -20 -40 10 0].'*pi/180;
 dtheta_1 = [0 0 0 0 0].'*pi/180;
 dtheta_2 = [5 -10 -15 -20 0].'*pi/180;
 
 % th1, th2, dth1, dth2, plotting, from_python
-[is_valid, opt_time, sumTorques] = findTrajectory(theta_1, theta_2, dtheta_1, dtheta_2, 1, 0);
-
+[is_valid, opt_time, sumTorques] = findTrajectory(theta_1, theta_2, dtheta_1, dtheta_2, 0, 0);
+sumTorques
 
